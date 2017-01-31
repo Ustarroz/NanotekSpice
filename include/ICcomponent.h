1272946c489bc42a0ec19d38facc04e0d6583d11
puilla_e <edouard.puillandre@epitech.eu>
2017-01-31 16:40:01 +0100

    reset main and ICcomponent to norm

--- include/ICcomponent.h
+++ include/ICcomponent.h
@@ -9,32 +9,32 @@
 
 namespace nts
 {
-    enum Tristate
- {
- UNDEFINED = (-true),
- TRUE = true,
- FALSE = false
- };
+  enum Tristate
+  {
+    UNDEFINED = (-true),
+    TRUE = true,
+    FALSE = false
+  };
 
- class IComponent
- {
- public:
- /// Compute value of the precised pin
- virtual nts::Tristate Compute(size_t pin_num_this = 1) = 0;
+  class IComponent
+  {
+   public:
+    /// Compute value of the precised pin
+    virtual nts::Tristate Compute(size_t pin_num_this = 1) = 0;
 
- /// Useful to link IComponent together
- virtual void SetLink(size_t pin_num_this,
- nts::IComponent &component,
- size_t pin_num_target) = 0;
+    /// Useful to link IComponent together
+    virtual void SetLink(size_t pin_num_this,
+			 nts::IComponent &component,
+			 size_t pin_num_target) = 0;
 
- ///// Print on terminal the name of the component and
- //// the state of every pin of the current component
- /// The output won’t be tested, but it may help you
- // as a trace.
- virtual void Dump(void) const = 0;
+    ///// Print on terminal the name of the component and
+    //// the state of every pin of the current component
+    /// The output won’t be tested, but it may help you
+    // as a trace.
+    virtual void Dump(void) const = 0;
 
- virtual ~IComponent(void) { }
- };
- }
+    virtual ~IComponent(void) { }
+  };
+}
 
 #endif //CMAKE_BUILD_DEBUG_ICCOMPONENT_H1
--- main.cpp
+++ main.cpp
@@ -2,10 +2,8 @@
 // Created by voyevoda on 31/01/17.
 //
 #include <iostream>
-#include <parser.h>
 
 int     main()
 {
-    nts::IParser test;
-    return (0);
+
 }
