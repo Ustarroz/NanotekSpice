848267481b0fd1d059a80fda0e5dededf25e1ae0
robin.ustarroz@epitech.eu <robin.ustarroz@epitech.eu>
2017-02-02 16:11:31 +0000

    class component

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 //
-// Created by voyevoda on 31/01/17.
+// main.cpp for bojou in /home/voyevoda/rendu/cpp/cpp_nanotekspice
 //
-#include <iostream>
+// Made by voyevoda
+// Login   <voyevoda@epitech.net>
+//
+// Started on  Thu Feb  2 15:19:50 2017 voyevoda
+// Last update Thu Feb  2 15:20:49 2017 voyevoda
+//
 
-int     main()
+int	main(int ac, char **av)
 {
 
 }
